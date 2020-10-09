accacaff3a35961dae53eb4bdbb583271a35ca69
agtgl <agtgl@LAPTOP-FTKS3D2R>
2020-10-09 20:06:50 -0300

    commit 2

--- TrabalhoTDD/Test/test/TestePagamentoBoleto.java
+++ TrabalhoTDD/Test/test/TestePagamentoBoleto.java
@@ -1,5 +1,29 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import fonte.Boleto;
+
 public class TestePagamentoBoleto {
 
+	@DisplayName("teste para a criação de um  boleto")
+	@Test
+	public void testBoleto() {
+		Boleto teste = new Boleto(001654,20102006,500.00);
+		Assertions.assertAll(teste,
+				() -> assertEquals(001654,teste.getCodigo()),
+				() -> assertEquals(20102006,teste.getData()),
+				() -> asserrtEquals(500.00,teste.getValor())
+		);		
+	}
+	
+	@DisplayName("teste para criação do cliente")
 }
--- TrabalhoTDD/src/fonte/Fatura.java
+++ TrabalhoTDD/src/fonte/Fatura.java
@@ -1,5 +1,33 @@
 package fonte;
 
 public class Fatura {
-	private Boleto ArrayList;
+	private int data;
+	private float valor_total;
+	private String nome;
+	
+	public Fatura(int data, float valor_total, String nome) {
+		this.data = data;
+		this.valor_total = valor_total;
+		this.nome = nome;
+	}
+	
+	public int getData() {
+		return data;
+	}
+	
+	public float getValor_total() {
+		return valor_total;
+	}
+	
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+	public void setValor_total(float valor_total) {
+		this.valor_total = valor_total;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
