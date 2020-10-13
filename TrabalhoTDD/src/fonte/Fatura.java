31d75b9353420dba501ee6cdf70ad5e9ab2e4812
agtgl <agtgl@LAPTOP-FTKS3D2R>
2020-10-12 22:42:55 -0300

    commit 4

--- TrabalhoTDD/src/fonte/Fatura.java
+++ TrabalhoTDD/src/fonte/Fatura.java
@@ -1,9 +1,13 @@
 package fonte;
 
+import java.util.Scanner;
+
 public class Fatura {
 	private int data;
 	private double valor_total;
 	private String nome;
+	public Boleto[] boletos;
+	private int count = 0;
 	
 	public Fatura(int data, double valor_total, String nome) {
 		this.data = data;
@@ -19,6 +23,10 @@ public class Fatura {
 		return valor_total;
 	}
 	
+	public String getNome() {
+		return nome;
+	}
+	
 	public void setData(int data) {
 		this.data = data;
 	}
@@ -29,5 +37,26 @@ public class Fatura {
 	
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public void addBoleto(Fatura fatura, int numBoletos, int codigoBoleto, int dataBoleto, double valorBoleto) {
+			fatura.boletos[count] = new Boleto(codigoBoleto,dataBoleto,valorBoleto);
+			this.count = count +1;
+		}
+	
+	public void addBoletoInterativo(Fatura fatura, int numBoletos) {
+		int codBoleto, dataBoleto;
+		double valorBoleto;
+		Scanner resp = new Scanner(System.in);
+		for(int i = 0;i<numBoletos;i++) {
+			System.out.println("Qual é o código do boleto?\n");
+			codBoleto = resp.nextInt();
+			System.out.println("Qual é a data do boleto?\n");
+			dataBoleto = resp.nextInt();
+			System.out.println("Qual é o valor do boleto?\n");
+			valorBoleto = resp.nextDouble();
+			fatura.boletos[count] = new Boleto(codBoleto,dataBoleto,valorBoleto);
+			this.count = count +1;
+		}
 	}
 }
